6ca9f0f31ebb069d84e2b919444140cd2577e390
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2019-10-04 15:59:10 +0530

    commit message

---
+++ Add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void add()
+{
+	int num1, num2,res;
+	printf("enter two number \n	");
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	res=num1+num2;
+	printf("sum of %d and %d ",num1, num2, res);	
+}
+int main()
+{
+	add();
+
+}
