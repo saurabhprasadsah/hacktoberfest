4f192583368ea2369bcc189990e587f14223a1dd
anchal029 <72280084+anchal029@users.noreply.github.com>
2020-10-16 10:48:41 +0530

    Update Add.c

--- Add.c
+++ Add.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-void add()
+void add(int num1, int num2)
 {
-	int num1, num2,res;
-	printf("enter two number \n	");
-	scanf("%d",&num1);
-	scanf("%d",&num2);
+	int res;
 	res=num1+num2;
-	printf("sum of %d and %d ",num1, num2, res);	
+	printf("sum of %d and %d is %d",num1, num2, res);	
 }
 int main()
 {
-	add();
+	int num1, num2;
+	printf("enter two number \n");
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	add(num1,num2);
 
 }
